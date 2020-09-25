c55aed83e73fd60df3f4340e537dc69e78e78413
pankaj-seecog <pankaj.7613@gmail.com>
2020-09-25 18:15:58 +0530

    1 file modified

--- src/List.js
+++ src/List.js
@@ -1,134 +1,133 @@
-import React from 'react';
-import data from "./data"
-import Company from './Company'
-class List extends React.Component{
+import React from "react";
+import data from "./data";
+import Company from "./Company";
+class List extends React.Component {
+  constructor(props) {
+    super(props);
 
-constructor(props){
-super(props);
+    //map the data start
+    data = data.map(item => {
+      return {
+        ...item,
+        lastSaved: new Date(),
+        editIndex: 0,
+        editStt: false
+      };
+    });
+    //map the data end
 
-//map the data start
-data = data.map((item)=>{
-  return {
-    ...item,
-    lastSaved : new Date(),
-    editIndex : 0,
-    editStt : false
+    this.state = {
+      companies: data,
+      editButton: false
+    };
   }
-})
-//map the data end
 
-this.state = {
-  companies : data,
-  editButton : false
-}
-}
+  editArr = index => {
+    this.setState({
+      editTxt: this.state.companies[index].name
+    });
+    let tmpArr = this.state.companies;
+    tmpArr = tmpArr.map((item, i) => {
+      if (i == index) {
+        return {
+          ...item,
+          editStt: !item.editStt,
+          editTxt: ""
+        };
+      }
+      return item;
+    });
+    this.setState({ companies: tmpArr });
+    this.setState();
+  };
 
-editArr = (index)=>{
-  this.setState({
-    editTxt : this.state.companies[index].name
-  })
-let tmpArr = this.state.companies;
-tmpArr = tmpArr.map((item,i)=>{
-if(i==index){
-return {
-  ...item,
-  editStt : !item.editStt,
-  editTxt : ""
-}
-}
-return item;
-})
-this.setState({companies : tmpArr});
-this.setState()
-}
+  updateFinally = (index, eleVal) => {
+    console.log(index, eleVal);
+  };
 
-updateFinally = (index,eleVal)=>{
-console.log(index,eleVal)
-}
+  saveData = evt => {
+    evt.preventDefault();
+    let tmpArr = this.state.companies;
+    tmpArr = tmpArr.map((item, i) => {
+      if (i == this.state.editIndex) {
+        return {
+          ...item,
+          name: this.state.editTxt,
+          editStt: false
+        };
+      }
+      return item;
+    });
+    this.setState({
+      companies: tmpArr
+    });
+  };
 
-saveData=(evt)=>{
-  evt.preventDefault()
-let tmpArr = this.state.companies;
-tmpArr = tmpArr.map((item,i)=>{
-if(i==this.state.editIndex){
-return {
-  ...item,
-  name : this.state.editTxt,
-  editStt : false,
-}
-}
-return item;
-})
-this.setState({
-  companies : tmpArr
-})
+  delRecord = index => {
+    if (confirm("Do you want to delete record ?")) {
+      let tmpArr = this.state.companies;
+      tmpArr.splice(index, 1);
+      this.setState({
+        companies: tmpArr
+      });
+    }
+  };
 
-}
-
-delRecord = (index)=>{
-let tmpArr = this.state.companies;
-tmpArr = tmpArr.splice(index,1);
-// tmpArr = tmpArr.map((item,i)=>{
-// if(i==index){
-// return {
-//   ...item,
-//   name : this.state.editTxt,
-//   editStt : false,
-// }
-// }
-// return item;
-// })
-this.setState({
-  companies : tmpArr
-})
-}
-
-render(){
-  return (
-    <div>
-<h1>Inside List</h1>
-<table className="table">
-<th>Company</th><th>Type</th><th>Last saved</th><th>Actions</th>
-{
-  this.state.companies.map((comp,i)=>{
-return (
-  <tr key={i}>
-<td>
-{
-(comp.editStt)?
-<div>
-<form onSubmit={this.saveData}>
-<input className="form-control" value={this.state.editTxt} onChange={(evt)=>{
-
-  this.setState({
-    editIndex : i,
-    editTxt : evt.target.value
-  })
-}}/>
-</form>
-</div>
-:<span>
-{comp.name}
-</span>
-}
-
-
-</td>
-<td>{comp.type}</td>
-<td>{}</td>
-<td>
-<button  className="btn btn-sm btn-primary" onClick={()=>this.editArr(i)}>Edit</button>
-<button  className="btn btn-sm btn-danger" onClick={()=>delRecord(i)}>Del</button>
-</td>
-  </tr>
-)
-  })
-}
-</table>
-    </div>
-  )
-}
-
+  render() {
+    return (
+      <div>
+        <h1>Inside List</h1>
+        <table className="table">
+          <th>Company</th>
+          <th>Type</th>
+          <th>Last saved</th>
+          <th>Actions</th>
+          {this.state.companies.map((comp, i) => {
+            return (
+              <tr key={i}>
+                <td>
+                  {comp.editStt ? (
+                    <div>
+                      <form onSubmit={this.saveData}>
+                        <input
+                          className="form-control"
+                          value={this.state.editTxt}
+                          onChange={evt => {
+                            this.setState({
+                              editIndex: i,
+                              editTxt: evt.target.value
+                            });
+                          }}
+                        />
+                      </form>
+                    </div>
+                  ) : (
+                    <span>{comp.name}</span>
+                  )}
+                </td>
+                <td>{comp.type}</td>
+                <td>{}</td>
+                <td>
+                  <button
+                    className="btn btn-sm btn-primary"
+                    onClick={() => this.editArr(i)}
+                  >
+                    Edit
+                  </button>
+                  <button
+                    className="btn btn-sm btn-danger"
+                    onClick={() => this.delRecord(i)}
+                  >
+                    Del
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </table>
+      </div>
+    );
+  }
 }
 
 export default List;
